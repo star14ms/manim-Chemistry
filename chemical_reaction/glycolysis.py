9fe526b60af0d185e6ab6be46265c7ea1ace35cf
Minseo <star14ms0@gmail.com>
2024-09-06 01:12:05 -0400

    Feat: Animate Glycolysis without Matching atoms and bonds

--- chemical_reaction/glycolysis.py
+++ chemical_reaction/glycolysis.py
@@ -6,48 +6,46 @@ from manim_chemistry import (
 )
 
 from pathlib import Path
-from manim.mobject.opengl.opengl_vectorized_mobject import OpenGLVGroup
 
 
-script_path = Path(__file__).absolute().parent
+script_path = Path(__file__).absolute().parent.parent
 files_path = script_path / "glycolysis"
+font = 'Comic Sans MS'
+molecules = [
+    'd-glucose',
+    'glucose-6-phosphate',
+    'fructose-6-phosphate',
+    'fructose-1,6-bisphosphate',
+    'dihydroxyacetone phosphate',
+    'glyceraldehyde-3-phosphate',
+    '1,3-bisphosphoglycerate',
+    '3-phosphoglycerate',
+    '2-phosphoglycerate',
+    'phosphoenolpyruvate',
+    'pyruvate',
+]
 
 
 # 2D Molecule example
 class Draw2DMorphine(Scene):
-
     # Two D Manim Chemistry objects require Cairo renderer
     config.renderer = "cairo"
     def construct(self):    
-        molecules = [
-            # 'd-glucose',
-            # 'glucose-6-phosphate',
-            # 'fructose-6-phosphate',
-            # 'fructose-1,6-bisphosphate',
-            # 'dihydroxyacetone phosphate',
-            # 'glyceraldehyde-3-phosphate',
-            # '1,3-bisphosphoglycerate',
-            # '3-phosphoglycerate',
-            # '2-phosphoglycerate',
-            'phosphoenolpyruvate',
-            'pyruvate',
-        ]
-
-        # from manim_chemistry.utils import mol_parser
-        # atoms1, bonds1 = mol_parser(files_path / (molecules[0] + '.sdf'))
-        # atoms2, bonds2 = mol_parser(files_path / (molecules[1] + '.sdf'))
-
+        initial_title = Text(molecules[0], font_size=64, font=font).to_edge(UP)
         initial_molecule = MMoleculeObject.from_mol_file(filename=files_path /(molecules[0] + '.sdf'))
-        # initial_molecule = GraphMolecule.build_from_mol(mol_file=files_path / (molecules[0] + '.sdf'))
-        self.play(Create(initial_molecule))
+        self.play(Write(initial_title), Create(initial_molecule), run_time=2)
+        self.wait(duration=2)
+        prev_title = initial_title
         prev_moleucule = initial_molecule
 
-        for molecule in molecules[1:]:
-            next_molecule = MMoleculeObject.from_mol_file(filename=files_path / (molecule + '.sdf'))
-            # next_molecule = GraphMolecule.build_from_mol(mol_file=files_path / (molecule + '.sdf'))
-            self.play(TransformMatchingShapes(prev_moleucule, next_molecule, key_map={}), run_time=4)
+        for title in molecules[1:]:
+            next_title = Text(title, font_size=64, font=font).to_edge(UP)
+            next_molecule = MMoleculeObject.from_mol_file(filename=files_path / (title + '.sdf'))
+            self.play(TransformMatchingShapes(prev_title, next_title), TransformMatchingShapes(prev_moleucule, next_molecule, key_map={}), run_time=2)
+            self.wait(duration=2)
             prev_moleucule = next_molecule
-    
+            prev_title = next_title
+
     def render(self):
         super().render(preview=True)
 
